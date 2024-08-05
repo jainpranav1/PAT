b1e4406366ad9b33bb6b885a009ab03d8a19f7a8
Pranav Jain <the.pranav.jain.1@gmail.com>
2024-08-04 19:47:26 -0500

    got three js in the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import * as THREE from "three";
+import { useState, useEffect, useRef } from "react";
 
 export default function Home() {
   const [userText, setUserText] = useState<string>();
   const [AIText, setAIText] = useState<string>();
 
-  function handleOnStart() {
+  function start() {
     const SpeechRecognition =
       window.SpeechRecognition || window.webkitSpeechRecognition;
     const recognition = new SpeechRecognition();
@@ -49,11 +50,44 @@ export default function Home() {
     recognition.start();
   }
 
+  // based off of this code:
+  // https://dev.to/omher/how-to-start-using-react-and-threejs-in-a-few-minutes-2h6g
+  const refContainer = useRef(null);
+  useEffect(() => {
+    // === THREE.JS CODE START ===
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild( renderer.domElement );
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    refContainer.current &&
+      refContainer.current.appendChild(renderer.domElement);
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate();
+  }, []);
+
   return (
     <div>
-      <button onClick={handleOnStart}>Start</button>
+      <button onClick={start}>Start</button>
       <p>User Text: {userText}</p>
       <p>AI Text: {AIText}</p>
+      <div ref={refContainer}></div>
     </div>
   );
 }
