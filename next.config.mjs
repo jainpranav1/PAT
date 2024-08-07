719e7bc3f9ef846371d2e11e3c76b3d85f990d98
Pranav Jain <the.pranav.jain.1@gmail.com>
2024-08-07 07:50:13 -0500

    prevented useeffect from running twice

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // reactStrictMode prevents useEffect from running twice on initial render
+  // https://rishabhsharma.bio/next-js-issue-useeffect-hook-running-twice-in-client-9fb6712f6362
+  reactStrictMode: false,
+};
 
 export default nextConfig;
