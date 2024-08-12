bdeed897a97591f042c3089d73978521493cb4de
Pranav Jain <the.pranav.jain.1@gmail.com>
2024-08-11 22:26:22 -0500

    added functionality to open up websites

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -1,9 +1,14 @@
 import { NextResponse } from "next/server";
 import dotenv from "dotenv";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import {
+  GoogleGenerativeAI,
+  FunctionDeclaration,
+  FunctionDeclarationSchemaType,
+  FunctionCall,
+} from "@google/generative-ai";
 import fs from "node:fs";
 import fsPromises from "node:fs/promises";
-
+import open from "open";
 dotenv.config();
 
 const gemini_api_key = process.env.GEMINI_API_KEY;
@@ -13,6 +18,37 @@ if (!gemini_api_key) {
 
 const genAI = new GoogleGenerativeAI(gemini_api_key);
 
+const openUrlDeclaration: FunctionDeclaration = {
+  name: "openUrl",
+  parameters: {
+    type: FunctionDeclarationSchemaType.OBJECT,
+    description: "Opens a url in the default browser.",
+    properties: {
+      url: {
+        type: FunctionDeclarationSchemaType.STRING,
+        description:
+          "A url. (Ex: https://www.google.com/, https://www.youtube.com/)",
+      },
+    },
+    required: ["url"],
+  },
+};
+
+const functions = {
+  openUrl: async ({ url }: { url: string }) => {
+    try {
+      await open(url);
+      return {
+        result: `opened ${url}`,
+      };
+    } catch (error) {
+      return {
+        result: `failed to open ${url}`,
+      };
+    }
+  },
+};
+
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
   systemInstruction: `
@@ -20,22 +56,45 @@ const model = genAI.getGenerativeModel({
   You are an AI assistant designed to provide information and perform tasks.
   No markdown (ex. asterisks) or emojis.
   `,
+  tools: [
+    {
+      functionDeclarations: [openUrlDeclaration],
+    },
+  ],
 });
 
-const converationText = fs.readFileSync("public/conversation.json", {
+const converation = fs.readFileSync("public/conversation.json", {
   encoding: "utf8",
 });
 
 const chat = model.startChat({
-  history: JSON.parse(converationText),
+  history: JSON.parse(converation),
 });
 
 export async function POST(request: Request) {
   const { userText } = await request.json();
 
-  const result = await chat.sendMessage(userText);
-  const response = await result.response;
-  const responseText = response.text();
+  const initialResult = await chat.sendMessage(userText);
+  const initialResponse = initialResult.response;
+  let responseText = initialResponse.text();
+  const initialResponseFuncCalls = initialResponse.functionCalls();
+  if (initialResponseFuncCalls) {
+    if (initialResponseFuncCalls.length > 0) {
+      const call = initialResponseFuncCalls[0];
+      const callResult = await functions[call.name](call.args);
+      const secondResult = await chat.sendMessage([
+        {
+          functionResponse: {
+            name: call.name,
+            response: callResult,
+          },
+        },
+      ]);
+      responseText = secondResult.response.text();
+    } else {
+      throw new Error("initialResponseFuncCalls is []");
+    }
+  }
 
   const history = await chat.getHistory();
 
